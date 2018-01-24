c01b5b79af5345abe19ff34e318737ee1182a294
Florian56 <net.lecraver@free.fr>
2018-01-24 21:30:24 +0100

    Il n'existe pas de fichier install ou uninstall.

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 module.exports = function(sails) {
     return {
-		install : require('./lib/install.js'),
-		uninstall : require('./lib/uninstall.js'),
 		exec : require('./lib/exec.js')
     };
 };
