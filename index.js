124c1bcddb356572636b56becc6788f20724a655
Florian56 <net.lecraver@free.fr>
2018-01-24 23:07:43 +0100

    Correction des fonctions.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 module.exports = function(sails) {
     return {
+		install : require('./lib/install.js'),
+		uninstall : require('./lib/uninstall.js'),
 		exec : require('./lib/exec.js')
     };
 };
